d00f9b52520630a84e2ca6aa8479bdc9828e5f1c
adisharma3107 <as777087@gmail.com>
2023-11-03 11:56:36 +0530

    Commit

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,17 @@
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        int n = 153;
+        int temp = n;
+        int p = 0;
+        while(n > 0){
+            int rem = n%10;
+            p = (p) + (rem * rem * rem);
+            n = n/10;
+        }
+        if(temp == p){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
---
+++ src/SqueezString.java
@@ -0,0 +1,18 @@
+public class SqueezString {
+
+    public static void main(String[] args)
+    {
+        String str = "    Geeks     for Geeks     ";
+        String op = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // Checking whether is white space or not
+            if (!Character.isWhitespace(ch)) {
+                op += ch;
+            }
+        }
+        System.out.println(op);
+    }
+}
