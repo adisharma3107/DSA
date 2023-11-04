b3586e16b779eb9b10a091c00a7f24e399655f98
adisharma3107 <as777087@gmail.com>
2023-11-04 16:33:29 +0530

    Commit

---
+++ src/DiamondPattern.java
@@ -0,0 +1,28 @@
+public class DiamondPattern {
+    public static void printPattern(int n){
+        int i,j;
+        for( i = 1 ; i <= n; i++){
+            for( j = 1 ; j <= n - i ; j++ ){
+                System.out.print(" ");
+            }
+            for( j = 1 ; j <= 2 * i - 1 ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for( i = n - 1; i >= 1 ; i-- ){
+            for( j = 1 ; j <= n - i ; j++ ){
+                System.out.print(" ");
+            }
+            for( j = 1 ; j <= 2 * i - 1 ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        printPattern(n);
+    }
+}
---
+++ src/RhombusPattern.java
@@ -0,0 +1,19 @@
+public class RhombusPattern {
+    public static void printPattern(int n){
+        int i,j;
+        for( i=1 ; i<=n ; i++){
+            for( j=1 ; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            for( j=1 ; j <= n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 6;
+        printPattern(n);
+    }
+}
