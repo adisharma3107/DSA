0e9ec81451e5e8ad17a42aaa1c821a3fe928d97e
Aditi Sharma <as777087@gmail.com>
2024-03-15 19:36:33 +0530

    Commit

--- src/PPatternRevertNumberedRightTriangle.java
+++ src/PPatternRevertNumberedRightTriangle.java
@@ -2,8 +2,8 @@ public class PPatternRevertNumberedRightTriangle {
 
     public static void pattern(int n){
         for(int i=0 ; i<n ; i++){
-            for(int j=n ; j<i ; j--){
-                System.out.print(j + " ");
+            for(int j=n ; j>i ; j--){
+                System.out.print(n - j + 1 + " ");
             }
             System.out.println();
         }
