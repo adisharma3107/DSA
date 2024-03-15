78e263a6a6362786f89bb61ac5a3a7bcf39b6a9a
Aditi Sharma <as777087@gmail.com>
2024-03-15 19:09:48 +0530

    Commit

---
+++ src/Interview.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+
+//public class Interview {
+
+//   public static int consecutiveOccurence(int [] arr, int n){
+//        int x = 0;
+//        for(int i=0; i <= n ; i++){
+//            for(int j = 0; j <= n; j++){
+//                if(arr[i] == arr[j]){
+//                    x++;
+//                }
+//            }
+//        }
+//        return x;
+//    }
+    
+//    List<Employee> employee = Arrays.asList(new Employee("A" , "Pune",500000),
+//            (new Employee("B" , "Pune",1000000)),
+//            (new Employee("A" , "Pune",500000)),200)
+//
+//
+//    public static void main(String[] args) {
+//        Map<String, Employee> topEmployee = employee.stream().collect(Collectors.toMap(
+//                e -> e.city,
+//                BinaryOperator.maxBy(Comparator.comparingInt(e -> e.salary))
+//        ));
+////salary        Optional<Employee> maxSalaryEmp = employee.stream().collect(Collectors.toMap());
+//        System.out.println(maxSalaryEmp.isPresent()? maxSalaryEmp);
+        
+        
+        
+        
+        
+//        int [] arr = {1,1,0,1,0,1,1,1};
+//        int n = arr.length;
+////        System.out.println(consecutiveOccurence(arr,n));
+//    }
+//}
---
+++ src/PPatternRevertNumberedRightTriangle.java
@@ -0,0 +1,16 @@
+public class PPatternRevertNumberedRightTriangle {
+
+    public static void pattern(int n){
+        for(int i=0 ; i<n ; i++){
+            for(int j=n ; j<i ; j--){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+}
