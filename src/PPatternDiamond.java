59250ca27d523a5225602d11dddcac1c6e1a5cc1
Aditi Sharma <as777087@gmail.com>
2024-03-16 12:15:52 +0530

    Commit

---
+++ src/PPatternDiamond.java
@@ -0,0 +1,42 @@
+public class PPatternDiamond {
+
+    public static void erectTriangle(int n){
+        for(int i = 0; i < n ; i++){
+            for(int j = 0; j < n-i-1; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0; j < 2*i+1; j++){
+                System.out.print("*");
+            }
+
+            for(int j = 0; j < n-i-1; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void revertTriangle(int n){
+        for(int i=0 ; i < n ; i++){
+            for(int j = 0; j<i ; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0; j < 2*n -(2*i +1); j++){
+                System.out.print("*");
+            }
+
+            for(int j=0; j<i ; j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        erectTriangle(n);
+        revertTriangle(n);
+    }
+}
