ca18a76fd2a8bbabdca3bdb6c5faa5f374a4f108
Aditi Sharma <as777087@gmail.com>
2024-03-21 10:53:02 +0530

    Commit

--- src/PPatternNumberedRightAngledTriangle.java
+++ src/PPatternNumberedRightAngledTriangle.java
@@ -1,12 +1,21 @@
 public class PPatternNumberedRightAngledTriangle {
 
     public static void pattern(int n){
+
+        int start =1;
+
         for(int i = 0; i < n ; i++){
-            for(int j = 0 ; j < i ; j++) {
-                if (j % 2 == 0) {
-                    System.out.print("1");
-                }
+
+            if(i % 2 == 0){
+                start = 1;
+            } else{
+                start = 0;
             }
+            for(int j = 0 ; j <= i ; j++) {
+                    System.out.print(start);
+                    start = 1 - start;
+            }
+            System.out.println();
         }
     }
 
